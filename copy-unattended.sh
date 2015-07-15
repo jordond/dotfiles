6c1a9c8d054503c319104e86a5da7e7ce5ecddfd
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-07-14 22:13:51 -0400

    Added param to unattended

--- copy-unattended.sh
+++ copy-unattended.sh
@@ -3,25 +3,25 @@
 ssh_dir="/etc/ssh"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$1/.oh-my-zsh" ]; then
   echo "Installing ohmyzsh..."
-  cd $HOME
+  cd $1
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh > /dev/null 2>&1
 fi
 
 echo "Copying custom zsh files"
-rm $HOME/.zsh_custom > /dev/null 2>&1
-ln -sv $dir/zsh_custom $HOME/.zsh_custom
+rm $1/.zsh_custom > /dev/null 2>&1
+ln -sv $dir/zsh_custom $1/.zsh_custom
 
 echo "Symlinking Dot files"
 cd $dir
-rm $HOME/.zshrc > /dev/null 2>&1
-rm $HOME/.tmux.conf > /dev/null 2>&1
-rm $HOME/.gitconfig > /dev/null 2>&1
+rm $1/.zshrc > /dev/null 2>&1
+rm $1/.tmux.conf > /dev/null 2>&1
+rm $1/.gitconfig > /dev/null 2>&1
 
-ln -sv $dir/zshrc $HOME/.zshrc
-ln -sv $dir/tmux.conf $HOME/.tmux.conf
-ln -sv $dir/git/gitconfig $HOME/.gitconfig
+ln -sv $dir/zshrc $1/.zshrc
+ln -sv $dir/tmux.conf $1/.tmux.conf
+ln -sv $dir/git/gitconfig $1/.gitconfig
 
 echo "Copying ssh files"
 sudo cp -v $dir/ssh/* $ssh_dir
