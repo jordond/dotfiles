86d79a97c3dd789490e5a630f92e63dad91ad142
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-08-10 15:46:21 -0400

    Changed around tmux config, and added some git aliases

--- distros/ubuntu/install_packages.sh
+++ distros/ubuntu/install_packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Install all base packages listed here
 
-BASE_PACKAGES="git curl wget zsh pv ngrep dstat ncdu mtr ppa-purge unity-tweak-tool htop sshfs autossh tmux iftop iotop vim "
+BASE_PACKAGES="git curl wget zsh pv ngrep dstat ncdu mtr ppa-purge unity-tweak-tool htop sshfs autossh tmux iftop iotop vim xclip tree "
 EXTRA_PACKAGES="deluge steam unrar p7zip-full"
-PPA_PACKAGES="plank numix-icon-theme-circle numix-plank-theme numix-gtk-theme numix-folders google-chrome-beta"
+PPA_PACKAGES="git plank numix-icon-theme-circle numix-plank-theme numix-gtk-theme numix-folders google-chrome-beta"
 
 NODEJS_URL="https://github.com/taaem/nodejs-linux-installer/releases/download/v0.3/node-install.sh"
 NODE_PACKAGES="node-inspector gulp yo bower generator-angular-fullstack generator-ng-poly"
@@ -15,6 +15,7 @@ echo "Adding PPAs..."
 
 sudo add-apt-repository ppa:ricotz/docky -y
 sudo add-apt-repository ppa:numix/ppa -y
+sudo add-apt-repository ppa:git-core/ppa -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 echo ""
@@ -35,6 +36,9 @@ echo ""
 echo "Installing Extra packages..."
 sudo apt-get install -y $EXTRA_PACKAGES
 echo ""
+
+echo "Installing tmux plugins"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Installing NodeJS..."
 wget -O - $NODEJS_URL | bash
