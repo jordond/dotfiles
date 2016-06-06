d42dc5f185fae85741a4d3f10c9d517e2ecc82a2
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-06-06 14:25:49 -0400

    Finishing touches on fish merge

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,3 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/home/jordon/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/jordon/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 for file in ~/.config/fish/conf.d/*.fish
     source $file
 end
--- fish_setup.sh
+++ fish_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 rm ~/.config/fish/fishfile
@@ -10,3 +12,7 @@ echo "Copying fish settings"
 ln -sv $dir/config/fish/fishfile ~/.config/fish/fishfile
 ln -sv $dir/config/fish/config.fish ~/.config/fish/config.fish
 ln -sv $dir/config/fish/functions/start_agent.fish ~/.config/fish/functions/start_agent.fish
+
+fish && fisher
+
+set fish_greeting
